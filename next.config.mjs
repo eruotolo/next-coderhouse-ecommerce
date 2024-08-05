289fa0707dee58e4a1a764b756de9dc1bde0483e
eruotolo <edgardoruotolo@outlook.com>
2024-08-05 17:34:16 -0400

    Tarea: Saliendo de Route Groups. Redirect /product a /shop Versión: 1.2 Fecha: 05-08-2024

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
             }
         ],
     },
+    async redirects() {
+        return [
+            {
+                source: '/product',
+                destination: '/shop',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
